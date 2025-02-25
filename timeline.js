a8b442928b71fedac324c05d6925a625042b35ce
johndoesstuff <johndoesstuff03@gmail.com>
2025-02-25 16:56:51 -0500

    reconfigure for div

--- timeline.js
+++ timeline.js
@@ -1,7 +1,8 @@
-const canvas = document.getElementById("canvas");
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.width * 1/3;
-const ctx = canvas.getContext("2d");
+//const canvas = document.getElementById("canvas");
+//canvas.width = canvas.clientWidth;
+//canvas.height = canvas.width * 1/3;
+const timelineContainer = document.getElementById("timeline");
+//const ctx = canvas.getContext("2d");
 const dayms = 1000*60*60*24;
 const yearms = 365*dayms;
 
@@ -35,45 +36,65 @@ function renderTimeline() {
 	if (timeline.elements.length > 0) timeline.endDisplay = timeline.elements.sort((a, b) => (a.start.getTime() - b.start.getTime()))[0].start;
 
 	let c = 0;
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (let i = timeline.endDisplay.getTime(); i <= timeline.startDisplay.getTime(); i += dayms) {
+	//ctx.clearRect(0, 0, canvas.width, canvas.height);
+	timelineContainer.innerHTML = "";
+	timelineContainer.style.position = "relative";
+	timelineContainer.style.display = "flex";
+	timelineContainer.style.flexDirection = "column";
+
+
+	/*for (let i = timeline.endDisplay.getTime(); i <= timeline.startDisplay.getTime(); i += dayms) {
 		let day = new Date(i);
 		let dayEnd = new Date(i + dayms);
-		ctx.fillStyle = (["#ddd", "#eee"])[c];
+		const col = day.getYear()%2 + day.getMonth()%2 + day.getDay()%2;
+		ctx.fillStyle = (['#fff', '#eee', '#ddd', '#ccc'])[col];
 		ctx.fillRect(dateToX(day), 0, dateToX(day) - dateToX(dayEnd), canvas.height);
 		c++;
 		c %= 2;
-	}
+	}*/
 
 	formatTimeline();
 
-	for (let i = 0; i < timeline.elements.length; i++) {
-		const timeframe = timeline.elements[i];
-		let start = dateToX(timeframe.start);
-		let end = dateToX(timeframe.end);
-		ctx.fillStyle = "blue";
-		ctx.fillRect(start, timeframe.z*30, end-start, 30);
+	for (let timeframe of timeline.elements) {
+		let eventDiv = document.createElement("div");
+		eventDiv.style.position = "absolute";
+		eventDiv.style.left = dateToX(timeframe.start) + "px";
+		console.log(eventDiv.style.left);
+		eventDiv.style.top = (timeframe.z * 30 + 3) + "px";
+		console.log(eventDiv.style.top);
+		eventDiv.style.width = (dateToX(timeframe.end) - dateToX(timeframe.start)) + "px";
+		eventDiv.style.height = "24px";
+		eventDiv.style.backgroundColor = "rgba(82, 108, 237, 0.78)";
+		eventDiv.style.border = "1px solid black";
+		eventDiv.style.borderRadius = "6px";
+
+		timelineContainer.appendChild(eventDiv);
 	}
 }
 
 function formatTimeline() {
-	let unformattedElements = timeline.elements;
+	let unformattedElements = [...timeline.elements].sort((a, b) => a.start - b.start); // Sort once
 	let z = 0;
+
 	while (unformattedElements.length > 0 && z < 10) {
 		let t = timeline.endDisplay.getTime();
-		while (t < timeline.startDisplay.getTime()) {
-			let soonest = unformattedElements.sort((a, b) => a.start.getTime() - b.start.getTime())
-			soonest = soonest.filter(e => e.start.getTime() > t);
-			if (soonest.length == 0) break;
-			soonest = soonest[0];
-			soonest.z = z;
-			t = soonest.end.getTime();
-			const index = unformattedElements.indexOf(soonest);
-			unformattedElements = unformattedElements.slice(0, index).concat(unformattedElements.slice(index + 1));
+		let remainingElements = [];
+
+		for (let i = 0; i < unformattedElements.length; i++) {
+			let e = unformattedElements[i];
+			if (e.start.getTime() >= t) {
+				e.z = z;
+				t = e.end.getTime();
+			} else {
+				remainingElements.push(e);
+			}
 		}
+
+		unformattedElements = remainingElements;
 		z++;
 	}
 }
+
 
 function dateToX(date) {
 	let datems = date.getTime();
@@ -82,7 +103,7 @@ function dateToX(date) {
 
 	let t = (datems-endms)/(startms-endms);
 
-	return canvas.width*t;
+	return timelineContainer.clientWidth*t;
 }
 
 renderTimeline();
